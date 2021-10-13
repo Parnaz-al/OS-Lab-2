3c8c8a033177efe018decb6746c80e7d50d309d4
fernnwehh <91968086+fernnwehh@users.noreply.github.com>
2021-10-13 16:52:12 +0330

    Add files via upload

--- tamrin3.py
+++ tamrin3.py
@@ -1,30 +1,38 @@
 import random
-print("choose!")
-print("✋->1")
-print("🤚->2")
-user=input()
-comp1=random.randint(1,2)
-comp2=random.randint(1,2)
-if(comp1==comp2):
-    if (user!=comp1):
-        print("you lose")
-        print("compueter1:",comp1,"computer2:",comp2)
-    
-    else:
-        print("no one lose")
-        print("compueter1:",comp1,"computer2:",comp2)
-    
-else:
-     if(user==comp1):
-        print("compurer2 lose")
-        print("compueter1:",comp1,"computer2:",comp2)
-    
-     else:    
-        print("computer1 lose")
-        print("compueter1:",comp1,"computer2:",comp2)
-    
+counter=5
+while(counter):
+    print("choose!")
+    print("✋->1")
+    print("🤚->2")
+    user=input()
+    comp1=random.randint(1,2)
+    comp2=random.randint(1,2)
+    counter=-1
+    if(comp1==comp2):
+        if (user!=comp1):
+            print("you lose")
+            print("compueter1:",comp1,"computer2:",comp2)
         
+        else:
+            print("no one lose")
+            print("compueter1:",comp1,"computer2:",comp2)
+        
+    else:
+        if(user==comp1):
+            print("compurer2 lose")
+            print("compueter1:",comp1,"computer2:",comp2)
+        
+        else:    
+            print("computer1 lose")
+            print("compueter1:",comp1,"computer2:",comp2)
+        
+            
 
+
+
+
+  
+        
 
 
 
